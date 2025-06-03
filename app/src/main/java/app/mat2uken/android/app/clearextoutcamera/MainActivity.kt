fdd103d71e7e8fd68da410d6c550ce5346b2093f
mat2uken <ku@yasagure.jp>
2025-06-03 12:51:46 +0900

    Initialize projetct

---
+++ app/src/main/java/app/mat2uken/android/app/clearextoutcamera/MainActivity.kt
@@ -0,0 +1,47 @@
+package app.mat2uken.android.app.clearextoutcamera
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.mat2uken.android.app.clearextoutcamera.ui.theme.ClearExtOutCameraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ClearExtOutCameraTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ClearExtOutCameraTheme {
+        Greeting("Android")
+    }
+}
